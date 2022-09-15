d301c09619b1ff98e0a51b7a816b6be9b593d435
keivy <keivysilva690@gmail.com>
2022-09-14 21:12:49 -0300

    usando metodo filter

---
+++ index.js
@@ -0,0 +1,10 @@
+const users = [
+  { id: 11, name: 'Adam', age: 23, group: 'editor' },
+  { id: 47, name: 'John', age: 28, group: 'admin' },
+  { id: 85, name: 'William', age: 34, group: 'editor' },
+  { id: 97, name: 'Oliver', age: 28, group: 'admin' }
+];
+
+let res = users.filter(it => it.name.includes('oli'));
+
+// res é []
